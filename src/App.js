7f41c8dbdba7972321d56f7565006b368e21999d
Sam Baraz <sam_94_bar@win.tu-berlin.de>
2021-06-08 15:29:25 +0200

    Jetzt kann man Favoriten hinzufügen. Sie werden in einer Liste abgespeichert und können wieder angezeigt werden. Die Infos sind auf die angezeigen Infos beschränkt, weil ich nicht weiß, wie man ein json richtig abspeichert.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const api = {
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 
+const favList = []
+
 function App() {
 
   const [query, setQuery] = useState('');
@@ -23,6 +25,12 @@ function App() {
     }
   }
 
+  const searchByFavorite = evt => {
+    // setQuery(evt.target.value);
+    const favInfo = favList.find(x => x.stadt === evt.target.value).info 
+    setWeather(favInfo)     
+  }
+
   const dateBuilder = (d) => {
     let months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
     let days = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
@@ -34,6 +42,42 @@ function App() {
 
     return `${day} ${date} ${month} ${year}`
   }
+
+  function addFavorite(item) {
+    var select = document.getElementById('fav-box-id'); 
+    var elementEnhalten = false;
+
+    Array.from(select).forEach(function(option_element) {
+      if (option_element.value == item) {
+        window.alert(`${item} ist bereits in den Favoriten enthalten!`);
+        elementEnhalten = true;
+        return;
+      } 
+    });
+    if (elementEnhalten) {
+      return
+    }
+
+    var el = document.createElement('option');
+    el.value = item;
+    el.textContent = item;
+    select.appendChild(el);
+    // window.alert(`Sie haben ${item} zu den Favoriten hinzugefügt.`)
+
+    favList.push({
+      stadt: `${weather.name}, ${weather.sys.country}`,
+      info: {
+        name: weather.name, 
+        sys: {country: weather.sys.country}, 
+        main: {temp: weather.main.temp}, 
+        weather: [{main: weather.weather[0].main}] 
+      }
+    });
+    console.log(favList[0])
+
+
+  }
+
 
   return (
     <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 16) ? ('app warm') : ('app')) : ('app')}>
@@ -54,15 +98,12 @@ function App() {
             Favoriten
           </div>
           <div className="körper">
-            <select className="auswahlbox" size="1">
-              <option>hier klicken ...</option>
-              <option>London</option>
-              <option>Madrid</option>
-              <option>Oslo</option>
-              <option>Berlin</option>
-              <option>London</option>
-              <option>Madrid</option>
-              <option>Oslo</option>
+            <select 
+              className="auswahlbox" 
+              size="1" id='fav-box-id' 
+              onChange={searchByFavorite}>
+              value={query}
+              //hier sit die Liste der Favoriten
             </select>
           </div>
         </div>
@@ -70,12 +111,12 @@ function App() {
         {(typeof weather.main != 'undefined') ? (
         <div className="info-box">
           <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="location" id="location-id">{weather.name}, {weather.sys.country}</div>
             <div className="date">{dateBuilder(new Date())}</div>
           </div>
           <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°C
+            <div className="temp" id="temp-id">
+                {Math.round(weather.main.temp)}°C
             </div>
             <div className="weather">
               {weather.weather[0].main}
@@ -85,7 +126,7 @@ function App() {
           <div className="add-favorite-box">
             <button 
               className="add-favorite-button"
-              onClick={() => window.alert('hallo')}
+              onClick={() => addFavorite(`${weather.name}, ${weather.sys.country}`)}
               >
                  <FcAcceptDatabase size="15px"  /> Zu Favoriten hinzufügen
             </button>
